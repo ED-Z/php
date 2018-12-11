5fcee3c9dcaddd8b9a9bc5815c5dd190de2d7479
ED-Z <45652440+ED-Z@users.noreply.github.com>
2018-12-11 22:59:26 +0800

    fuzhou weather

---
+++ weather.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+<?php
+echo "<h1> Fuzhou Weather Today! </h1>";
+$file=fopen("1.txt","w");
+$baidu=file_get_contents('http://www.weather.com.cn/weather/101230101.shtml');
+fwrite($file,$baidu);
+preg_match("/\<li class=\"sky skyid lv3 on\"\>(.*?)\<\/li\>/s",$baidu,$wea);
+preg_match("/title=\"(.*?)\"\sclass=\"NW\"\>/",$wea[1],$fx);
+print_r($wea[1]);
+print_r($fx[1]);
+fclose($file);
+?>
+
+</body>
+</html>
