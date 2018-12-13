9694e52d8413fbe2f094faabeb57ff67eb143d9d
ED-Z <45652440+ED-Z@users.noreply.github.com>
2018-12-13 23:45:06 +0800

    修改风向

--- weather.php
+++ weather.php
@@ -1,19 +1,21 @@
 <html>
-<body>
 <head>
 <title>Fuzhou weather</title>
 </head>
+<body>
+
 <?php
 echo "<h1> Fuzhou Weather Today! </h1>";
 $file=fopen("1.txt","w");
 $baidu=file_get_contents('http://www.weather.com.cn/weather/101230101.shtml');
 fwrite($file,$baidu);
 preg_match("/\<li class=\"sky skyid lv3 on\"\>(.*?)\<\/li\>/s",$baidu,$wea);
-preg_match("/title=\"(.*?)\"\sclass=\"NW\"\>/",$wea[1],$fx);
+preg_match("/\<p class=\"win\"\>\s\<em\>\s\<span title=\"(.*?)\"\sclass=(.*?)\<\/p\>/s",$wea[1],$fx);
 print_r($wea[1]);
 print_r($fx[1]);
 fclose($file);
 ?>
+
 
 </body>
 </html>
