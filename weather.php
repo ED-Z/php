439f1d06d4067e40cdbe7c5b5b85e37aced86233
ED-Z <45652440+ED-Z@users.noreply.github.com>
2018-12-16 22:53:59 +0800

    修改错误

--- weather.php
+++ weather.php
@@ -9,7 +9,7 @@ echo "<h1> Fuzhou Weather Today! </h1>";
 $file=fopen("1.txt","w");
 $baidu=file_get_contents('http://www.weather.com.cn/weather/101230101.shtml');
 fwrite($file,$baidu);
-preg_match("/\<li class=\"sky skyid lv3 on\"\>(.*?)\<\/li\>/s",$baidu,$wea);
+preg_match("/\<li class=\"sky skyid lv\d on\"\>(.*?)\<\/li\>/s",$baidu,$wea);
 preg_match("/\<p class=\"win\"\>\s\<em\>\s\<span title=\"(.*?)\"\sclass=(.*?)\<\/p\>/s",$wea[1],$fx);
 print_r($wea[1]);
 print_r($fx[1]);
